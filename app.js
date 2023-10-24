fabab6f6958797feab25f61aeb16f9a1877ff2d7
huntr <satria.pandega@gmail.com>
2023-10-24 21:58:53 +0700

    testing express-rate-limit

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const cors = require("cors");
+const rateLimit = require("express-rate-limit").rateLimit;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Use an external store for consistency across multiple server instances.
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(limiter);
 app.get("/test", (req, res) => {
   res.status(200).json({
     message: "Test successful",
